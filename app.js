2f03c13864b56b4c6025062e897f5f984f35f9ab
Alex <Alex@Alex-Mac.home>
2020-06-15 15:18:20 -0400

    re-commenting stuff I had taken out accidentally

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// pulling data from api
+
 const url = "https://jservice.io/api/random"
+
+// assigning variables by grabbing elements with the DOM
 
 const button = document.getElementById('button')
 const questionBox = document.getElementById('questionBox')
 const answerBox = document.getElementById('answerBox')
 const trebek = document.getElementsByClassName('trebek')
+
+// clears out innerHTML from Q and A boxes before repoppulating
+// asyncronously grabs api response & assigning specific data types to variables for
+// category, question, answer 
 
 const getQuestion = async () => {
   try {
@@ -26,5 +34,8 @@ const getQuestion = async () => {
   }
 }
 
+// add EventListener to call the getQuestion function
+// again clearing the previous one, and repopulating
+// with a new question and answer
 
 button.addEventListener('click', getQuestion)
