efc5d92513f55eb4d7386e411f180472196de779
Alex Beckerman <sonificus@gmail.com>
2020-10-18 17:49:19 -0400

    update

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// pulling data from api
+// setting API url,
+// assigning variables to certain items, 
+// selected via DOM by Id / ClassnName:
+// ++++++++++++================================================-------------
 
-const url = "https://jservice.io/api/random"
-
-// assigning variables by grabbing elements with the DOM
+const url = 'https://jservice.io/api/random'
 
 const button = document.getElementById('button')
 const questionBox = document.getElementById('questionBox')
 const answerBox = document.getElementById('answerBox')
 const trebek = document.getElementsByClassName('trebek')
 
-// clears out innerHTML from Q and A boxes before repoppulating
-// asyncronously grabs api response & assigning specific data types to variables for
+// +++++++++++================================================-------
+// clear out innerHTML from questionBox & answerBox 
+// before re-populating w. new question/answer pairs.
+// asynchronously grab API response
+// create variables for:
 // category, question, answer 
+// +++++======================================================---
 
 const getQuestion = async () => {
   try {
@@ -22,20 +27,24 @@ const getQuestion = async () => {
     let category = response.data[0].category.title
     let question = response.data[0].question
     let answer = response.data[0].answer
+    
     console.log(category)
     console.log(question)
     console.log(answer)
    
     questionBox.innerHTML += question;
     answerBox.innerHTML += answer;
-
-  } catch (error) {
+  } catch(error) {
     console.log(`This is your error: ${error}`)
   }
 }
 
-// add EventListener to call the getQuestion function
-// again clearing the previous one, and repopulating
-// with a new question and answer
-
 button.addEventListener('click', getQuestion)
+
+// ========================================
+// added EventListener 
+// to call getQuestion() function
+// on the action 'click',
+// again, clearing the previous question
+// and repopulating
+// with a new question / answer pair.
