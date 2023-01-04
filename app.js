2ccadd467b0c904de310704d465f909a39268006
Alex Beckerman <sonificus@gmail.com>
2023-01-04 03:31:40 -0500

    some changes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const url = 'https://jservice.io/api/random'
-
 const button = document.getElementById('checkButton')
 const answerButton = document.getElementById('answerButton')
 const questionButton = document.getElementById('questionButton')
@@ -9,24 +8,22 @@ const questionBox = document.getElementById('questionBox')
 const answerBox = document.getElementById('answerBox')
 const trebek = document.getElementsByClassName('trebek')
 
-let streak = 0 // start correct-answer streak  at zero
-//______________________
-//-----------------------------------------------
-// model or speech bubble change as alternative to window.alert 
-// regex for checkAnswer to remove parentheses, allow for leaving out the, a, an, etc. parts, make lower case 
-// implement streak counter and perhaps other scoring - add/delete value from correct/incorrect answers to track score?
-// change date to nicer format for air-date (i.e. August 14, 1984)
-//-----------------------------------------
-//_____________________
-const getQuestion = async() => {
-  try {
-    // clear previous question data:
-    categoryBox.innerHTML = '';
-    questionBox.innerHTML = '';
-    answerBox.innerHTML = '';
-    userInput.value = '';
-    // fetch data from api and assign to values for use in app
-    // will set to $100 as default question value in case null is returned by API due to missing data:
+// initialize streak at zero
+let streak = 0
+
+// clear data from previous question by resetting innerHTML for each box to an empty string
+// fetch data from api and assign values to our variables for use in app
+// will set to $100 as default question value in case null is returned by API due to missing data
+// place question/answer and other data into app:
+
+const getQuestion = async() => 
+{
+  try 
+  {
+    categoryBox.innerHTML = ''
+    questionBox.innerHTML = ''
+    answerBox.innerHTML = ''
+    userInput.value = ''
     let response = await axios.get(url)
     let category = response.data[0].category.title
     let question = response.data[0].question
@@ -34,13 +31,13 @@ const getQuestion = async() => {
     let value = response.data[0].value || '$100' 
     let date = new Date(response.data[0].airdate) 
     let datestring = (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear()
-    // place question/answer and other data into app:
-    categoryBox.innerHTML += category.toUpperCase() + `<br/> for $` + value + `<br/>` + '(asked on ' + datestring + ' )';
-    questionBox.innerHTML += question;
-    answerBox.innerHTML += answer;
-    answerBox.style.display = "none";
+    categoryBox.innerHTML += category.toUpperCase() + `<br/> for $` + value + `<br/>` + '(asked on ' + datestring + ' )'
+    questionBox.innerHTML += question
+    answerBox.innerHTML += answer
+    answerBox.style.display = "none"
   } 
-  catch(error) {
+  catch(error) 
+  {
     console.log(`Question fetch failed: ${error}`)
   }
 }
@@ -48,10 +45,14 @@ const getQuestion = async() => {
 // the showHideAnswer button is basically a TOGGLE to show you what the answer isArray
 // for now this is helping a lot with figuring out what needs to be modified
 // to increase accuracy of checkAnswer() function & eliminate undesired non-matches
-const showHideAnswer = () => {
-  if (answerBox.style.display === "none") {
+const showHideAnswer = () => 
+{
+  if (answerBox.style.display === "none") 
+  {
     answerBox.style.display = "flex"; 
-  } else {
+  } 
+  else 
+  {
     answerBox.style.display = "none";
   } 
 } 
@@ -59,71 +60,59 @@ const showHideAnswer = () => {
 answerButton.addEventListener('click', showHideAnswer)
 questionButton.addEventListener('click', getQuestion)
 
-// TRANSFORMATIONS TO INCREASE ACCURACY
-// if first word in answer is 'a' 'the' 'an' remove that word
-// if first word is 'a' 'the' 'an' remove from userInput submission
-// if first word is 'a' 'the' 'an' remove from answer
-// ----------------------------------
-// once transformations are made 
-// then compare/check answer vs submission last
-// probably is best approach
-
-const checkAnswer = () => {
+// if correct, alert success & streak counter increments +1
+// window.alert("I am Canadianly delighted to report you are correct, sir or madame! I like how you think!!!");
+// if incorrect, user has inputted an incorrect string
+// streak resets when incorrect
+const checkAnswer = () => 
+{
   let answer = answerBox.innerHTML
   let answerTrimmed = answer.trim()
+  console.log(answer)
+  console.log(answerTrimmed)
 
-console.log(answer)
-console.log(answerTrimmed)
+  if 
+  (  
+  userInput.value.toLowerCase() == answer.toLowerCase().replace('\\', '') || 
+  userInput.value.toLowerCase() == answer.toLowerCase().replace('\\', '')
+  ) 
+    {
+      streak++
+      console.log(streak)
+      questionBox.innerHTML = "I am Canadianly delighted to report you are correct, sir or madame! I like how you think!!!  You are beautiful and well-liked by all..";
+      categoryBox.innerHTML = "CORRECT!!!";
+      answerBox.innerHTML = "Correct Answer Streak: " + streak;
+    } 
+    else 
+    {  
+      categoryBox.innerHTML = "NOPE!!!";
+      questionBox.innerHTML = "I'm sorry, that's either incorrect or the judges are...  It could be them, they're a bit drunk...";
+      answerBox.style.display = "flex"
+      answerBox.innerHTML = `Correct answer:` + `<br/>` + `<br/>` +
+                              answer + `<br/>` + `<br/>` +
+                            `STREAK RESET!!!`;
 
-    if (userInput.value.toLowerCase() == answer.toLowerCase().replace('\\', '') ||  
-        userInput.value.toLowerCase() == answer.toLowerCase().replace('\\', '')) {
-// if correct, alert success & streak counter increments +1
-        streak++; 
-
-console.log(streak)
-
-        questionBox.innerHTML = "I am Canadianly delighted to report you are correct, sir or madame! I like how you think!!!  You are beautiful and well-liked by all..";
-        categoryBox.innerHTML = "CORRECT!!!";
-        answerBox.innerHTML = "Correct Answer Streak: " + streak;
-// window.alert("I am Canadianly delighted to report you are correct, sir or madame! I like how you think!!!");
-      } else {  
-// if incorrect, user has inputted an incorrect string
-        categoryBox.innerHTML = "NOPE!!!";
-        questionBox.innerHTML = "I'm sorry, that's either incorrect or the judges are...  It could be them, they're a bit drunk...";
-        answerBox.style.display = "flex"
-        answerBox.innerHTML = `Correct answer:` + `<br/>` + `<br/>` +
-                               answer + `<br/>` + `<br/>` +
-                              `STREAK RESET!!!`;
-// streak resets when incorrect
-        streak = 0; 
-      }
-
+      streak = 0; 
+    }
 }
 
-// create a SANITIZE function to clean up the correct answer from API
-// as well as USER INPUT, for final comparison.
+// create a SANITIZE function to clean up the correct answer from API and USER INPUT for final comparison.
 // THINGS THIS SHOULD DO ARE:
   // if first word is 'the', 'an', 'a' or other articles like that, remove the first word
   // trim any extra spaces from beginning or end, using TRIM (especially important because space will often be auto-added on mobile keyboards)
   // remove any quotes or escape characters
   // remove capitalization (already doing this)
   // use some sort of regex to accomplish a bunch of these / removing special characters, etc.
+
   
-
-
-
-//
-//
 // Create variables for:
 // dialog = document.createElement("div");
 // wordBubble = document.createElement("div")
 // correct = document.createElement("div")
 
 
-
-
-
 // window.alert("I'm sorry, that's either incorrect or the judges are...  It definitely could be them, they're a little drunk..."); //reset streak counter
+
 
 // empty content from previous question
 // get new question from api
@@ -132,6 +121,7 @@ console.log(streak)
 // could also randomize or minimize api calls by incrementing to other questions in array
 // display category, question value, and question
 // answerBox set to display = "none" until user chooses to reveal answer with showHideAnswer() function:
+
 
 // _____NEED TO:_____
     // remove 'the' from answer (also a, an, etc?)
