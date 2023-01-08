e58d0ef720fe372ce1cdca63a4ba7f6e60aae1a0
Alex Beckerman <sonificus@gmail.com>
2023-01-08 14:52:36 -0500

    making moves - cleaning up code & adding functionality, console logs, etc.

--- app.js
+++ app.js
@@ -17,41 +17,50 @@ const trebek = document.getElementsByClassName('trebek')
 
 // initialize correct Answer Streak at zero
 let streak = 0
-
+console.log(`Welcome to Jeopardish!!!`)
+console.log(`Click the "new question" button to grab a random Jeopardy question & test your knowledge.`)
+console.log(`Multiple correct answers in a row will start a streak...`)
+console.log(`...but get one wrong & the streak will reset.`)
+console.log("Let's see how many correct answers you can string together! ")
+console.log(`Streak is currently at ` + streak )
+console.log("HAVE FUN YA MANIAC!")
   // clear data from previous question by resetting innerHTML for each box to an empty string
   // fetch data from api and assign values to our variables for use in app
   // will set to $100 as default question value in case null is returned by API due to missing data
-  // place question/answer and other data into app:
+
+// function to grab question from api:
 const getQuestion = async() => {
-  try 
-    {
+  try {
     categoryBox.innerHTML = ''
     questionBox.innerHTML = ''
     answerBox.innerHTML = ''
     userInput.value = ''
-
+    // grab data:
     let response = await axios.get(url)
+    console.log("question grabbed: ")
+    console.log(response.data[0])
+    // set variables for the info to display in word-bubble
     let category = response.data[0].category.title
     let question = response.data[0].question
     let answer = response.data[0].answer 
     let value = response.data[0].value || '$100' 
-    let date = new Date(response.data[0].airdate) 
+    let date = new Date(response.data[0].airdate)
+    // interpolating date data into a string
     let datestring = (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear()
-
-    categoryBox.innerHTML += category.toUpperCase() + `<br/> for $` + value + `<br/>` + '(asked on ' + datestring + ' )'
-    questionBox.innerHTML += question
-    answerBox.innerHTML += answer
+    // display in word-bubble
+    categoryBox.innerHTML = category.toUpperCase() + `<br/> for $` + value + `<br/>` + '(asked on ' + datestring + ' )'
+    questionBox.innerHTML = question
+    answerBox.innerHTML = answer
+    // set answer as invisible until revealed
     answerBox.style.display = "none"
-    } 
-  catch(error) 
-    {
+  } 
+  catch(error) {
       console.log(`Question fetch failed: ${error}`)
-    }
+  }
 }
 
-// the showHideAnswer button TOGGLES to show you what the answer isArray
-// for now this is helping a lot with figuring out what needs to be modified
-// to increase accuracy of checkAnswer() function & eliminate undesired non-matches
+
+// answer reveal toggle switch 
 const showHideAnswer = () => {
   if (answerBox.style.display === "none") 
     {
@@ -66,6 +75,8 @@ const showHideAnswer = () => {
 // add event listeners to buttons to trigger functionality
 answerButton.addEventListener('click', showHideAnswer)
 questionButton.addEventListener('click', getQuestion)
+// _____TO DO_____
+// need to add event listener for pressing enter in the text input field will submit, as well as the checkAnswer button
 
 // FUNCTION TO CHECK IF USER ANSWER MATCHES CORRECT ANSWER FROM API:
   // if correct, alert success & streak counter increments +1
@@ -74,10 +85,9 @@ questionButton.addEventListener('click', getQuestion)
   // streak resets when incorrect
 const checkAnswer = () => {
   let answer = answerBox.innerHTML
+  console.log( {answer} )
   let answerTrimmed = answer.trim
-  // CONSOLE LOGGING TO TEST
-  console.log(answer)
-  console.log(answerTrimmed)
+  console.log( {answerTrimmed} )
 
   if 
     (
@@ -87,28 +97,25 @@ const checkAnswer = () => {
       {
         //increment streak
         streak++
-        console.log(streak)
+        console.log( {streak} )
 
         questionBox.innerHTML = "I am Canadianly delighted to report you are correct, sir or madame! I like how you think!!!  You are beautiful and well-liked by all..";
-        categoryBox.innerHTML = "CORRECT!!!";
+        categoryBox.innerHTML = "";
         answerBox.innerHTML = "Correct Answer Streak: " + streak;
       } 
     else 
       {  
-        categoryBox.innerHTML = "NOPE!!!";
+        categoryBox.innerHTML = "";
         questionBox.innerHTML = "I'm sorry, that's either incorrect or the judges are...  It could be them, they're a bit drunk...";
         answerBox.style.display = "flex"
-        answerBox.innerHTML = `Correct answer:` + `<br/>` + `<br/>` +
+        answerBox.innerHTML = `The correct answer was..` + `<br/>` + `<br/>` +
                                 answer + `<br/>` + `<br/>` +
-                              `STREAK RESET!!!`;
+                              `STREAK RESET TO ZERO!`;
         // reset streak
-        streak = 0; 
+        streak = 0;
+        console.log("streak reset!")
+        console.log( {streak} )
       }
-
-
-
-
-
 
 }
 
