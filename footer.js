707b0cac49850c96669ff79f41bc690c4abdd41c
Alex Beckerman <alexlbeckerman@gmail.com>
2023-10-18 18:57:09 -0400

    minor change

--- footer.js
+++ footer.js
@@ -1,6 +1,5 @@
-
-   <div class="container-fluid">
-      <footer class="bs-footer">
-        <small>this game is a work of fiction about facts. any similarities to real persons Canadian or otherwise are purely coincidental. </small>
-      </footer>
-    </div>
+<div class="container-fluid">
+  <footer class="bs-footer">
+    <small> This game is a work of trivia. Any similarities to real persons, Canadian, moustachio'd, living, dead or otherwise are purely coincidental. </small>
+  </footer>
+</div>
